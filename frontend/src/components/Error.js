53d028cdd1d613047aac938bf68d25448e00ba09
Saadh S <atextbooksituation@gmail.com>
2021-08-21 11:00:27 -0400

    Error page - forgot to fix the redirects in backend and add frontend route
    - Committing one file at a time
    - Didn’t commit in real time
    - Nothing has been tested

---
+++ frontend/src/components/Error.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+
+function Error( props ) {
+  useEffect( () => { 
+      setTimeout( () => 
+        props.history.push( '/' ), 6000 
+      );
+  } );
+
+  return (
+    <div>
+      <p>Something went wrong. Have to go back to logging in.</p>
+      <p><a href="/">Click here to do that</a> or you'll be redirected to the home page in a couple of seconds</p> 
+    </div>
+  );
+};
+
+export default Error;
