543ad23f465a33d7887ae9f05ae5de33f8301d9d
Saadh S <atextbooksituation@gmail.com>
2021-09-08 09:41:20 -0400

    Fixed up Category. Handling of clicking is done in parent

--- frontend/src/components/RolesDashboard/Category.js
+++ frontend/src/components/RolesDashboard/Category.js
@@ -1,50 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Role from "./Role";
 
-function Category( { name, description, roles, handleRole, loading, filter } ) {
+function Category( { name, description, roles, handleRole, loading } ) {
   const [ catRoles, setCatRoles ] = useState( [] );
   const cssName = name.trim().split( ' ' ).join( '-' ).toLowerCase();
 
   useEffect( () => {
-    prepRoles();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    setCatRoles( roles );
   }, [ roles ] );
 
-  const firstRun = () => {
-    if ( name && Array.isArray( roles ) && roles.length > 0 ) {
-      if ( filter ) {
-        setCatRoles( filterRoles( roles, name ) );
-      } else if ( !filter ) {
-        setCatRoles( roles );
-      };
-    };
-  };
-
-  const filterRoles = ( roles, name ) => 
-    roles.filter( role => 
-      role.category === name
-    );
-
-  const handleCatRole = ( e ) => {
-    const id = e.target.id;
-    const index = catRoles.findIndex( role => role.id === id );
-    if ( index === -1 ) {
-      return;
-    };
-    const currRole = catRoles[ index ];
-    setCatRoles( prev => prev.splice( index, 1 ) );
-    handleRole( currRole );
+  const handleTheRole = ( e ) => {
+    handleRole( e.target.id );
   };
 
   return (
   <div className="card">
-      
       { loading && 
       <p className="text-center text-secondary">
         Loading...
       </p>
       }
 
+      { !loading && 
+      <>
       <div 
         id={ `${ cssName }-roles-header` }
         className="card-header" 
@@ -64,22 +42,24 @@ function Category( { name, description, roles, handleRole, loading, filter } ) {
         className="collapse show" 
       >
         { 
-          <div className="card-body" id={ `${ cssName }-roles-container` }>
-            { Array.isArray( catRoles ) && catRoles.map( ( role, index ) => ( 
-              <Role 
-                key={ index } 
-                category={ cssName } 
-                id={ role.id } 
-                name={ role.name } 
-                description={ role.description } 
-                color={ role.color } 
-                onClick={ handleCatRole } 
-              />
-            ) ) }
-          </div>
+        <div className="card-body" id={ `${ cssName }-roles-container` }>
+          { Array.isArray( catRoles ) && catRoles.map( ( role, index ) => ( 
+            <Role 
+              key={ index } 
+              category={ cssName } 
+              id={ role.id } 
+              name={ role.name } 
+              description={ role.description } 
+              color={ role.color } 
+              onClick={ handleTheRole } 
+            />
+          ) ) }
+        </div>
         }
       </div> {/* <!-- #collapse-cat-roles --> */}
     {/* <!-- .card --> */}
+    </>
+    }
     </div>
   );
 };
