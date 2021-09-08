2c3de7785e120126762a2054628b68efbb1017cc
Saadh S <atextbooksituation@gmail.com>
2021-09-08 11:19:55 -0400

    Mostly cosmetic or minor changes in logic.js
    - Added if qualifier to mergedRoles function.
    - Adjusted the code appearance across the file, made things look better to me

--- server/logic.js
+++ server/logic.js
@@ -17,13 +17,12 @@ const {
 
 const grabRoles = async () => {
   let response;
+  const fullUrl = `${ discord.api }guilds/${ guild.id }/roles`;
   try {
     response = await axios.get( 
-      discord.api + 'guilds/' + guild.id + '/roles', {
-        headers: {
-          'Authorization': 'Bot ' + bot.token
-        }
-      } 
+      fullUrl, { headers: {
+        'Authorization': 'Bot ' + bot.token
+      } } 
     );
   } catch ( err ) {
     console.error( 'discord guild roles fail, status', err.response.status );
@@ -43,11 +42,11 @@ const grabRoles = async () => {
 const getDiscordUser = async ( bearerToken ) => {
   let user;
   try {
-    user = await axios.get( discord.api + 'users/@me', {
-      headers: {
+    user = await axios.get( 
+      discord.api + 'users/@me', { headers: {
         'Authorization': 'Bearer ' + bearerToken 
-      }
-    } );
+      } } 
+    );
   } catch ( err ) {
     console.error( 'discord user fail, status', err.response.status );
     console.error( 'discord user fail, data', err.response.data );
@@ -59,14 +58,11 @@ const getDiscordUser = async ( bearerToken ) => {
 const getDiscordUserGuilds = async ( bearerToken ) => {
   let guilds;
   try {
-    guilds = await axios.get( discord.api + 'users/@me/guilds', {
-      headers: {
+    guilds = await axios.get( 
+      discord.api + 'users/@me/guilds', { headers: {
         'Authorization': 'Bearer ' + bearerToken
-      }
-    } );
-    if ( guilds.status === 401 ) {      
-      return false;
-    };
+      } } 
+    );
   } catch ( err ) {
     console.error( 'discord user guild fail, status', err.response.status );
     console.error( 'discord user guild fail, data', err.response.data );
@@ -89,6 +85,7 @@ const grabGuildUser = async ( guildId, userId ) => {
   };
   return guildUser.data;
 };
+
 const grabUser = async ( req, res ) => {
   if ( !securityCheck( req.session.bearer_token, req.headers.bearer_token ) ) { res.status( 401 ).end(); return; }
 
@@ -104,7 +101,19 @@ const grabUser = async ( req, res ) => {
     username: user.username, 
     discriminator: user.discriminator, 
     avatar: user.avatar, 
-    isAZoomer: isGuildMember( guilds, guild.id ) 
+    // isGuildMember: isGuildMember( guilds, guild.id ), 
+
+    flags: user.flags, 
+    publicFlags: user.public_flags, 
+
+    banner: user.banner, 
+    bannerColor: user.banner_color, 
+    accentColor: user.accent_color, 
+
+    nick: guildUser.nick, 
+    premium: guildUser.premium_since, 
+    joined: guildUser.joined_at, 
+    pending: guildUser.is_pending  
   };
 };
 
@@ -112,14 +121,21 @@ const mergeNotionDiscordRoles = async () => {
   const notionRoles = await addNotionCategoriesToRoles();
   const discordRoles = await grabRoles();
 
-  const mergedRoles = notionRoles.map( solo => { 
-    const currRole = discordRoles.find( han => 
-      han.name.toLowerCase() === solo.name.toLowerCase() 
-    );
-    return currRole != undefined ? { ...solo, ...currRole } : null;
-  } )
+  if ( 
+    Array.isArray( notionRoles ) && notionRoles.length > 0 
+    && 
+    Array.isArray( discordRoles ) && discordRoles.length > 0 
+  ) {
+    const mergedRoles = notionRoles.map( solo => { 
+      const currRole = discordRoles.find( han => 
+        han.name.toLowerCase() === solo.name.toLowerCase() 
+      );
+      return currRole != undefined ? { ...solo, ...currRole } : null;
+    } );
 
-  return mergedRoles.filter( han => han != null );
+    return mergedRoles.filter( han => han != null );
+  };
+  return false;
 };
 
 const filterRoles = async ( rolesArr = [] ) => {
