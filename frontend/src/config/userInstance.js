48af391118bc59e210ccf52e3b53ea6491052474
Saadh S <atextbooksituation@gmail.com>
2021-09-08 10:25:33 -0400

    Forgot to add the user empty instance before

---
+++ frontend/src/config/userInstance.js
@@ -0,0 +1,21 @@
+const user = {
+  id: null,
+  username: null,
+  discriminator: null,
+  avatar: null,
+  isGuildMember: null,
+
+  flags: null,
+  publicFlags: null,
+
+  banner: null,
+  bannerColor: null,
+  accentColor: null,
+
+  nick: null,
+  premium: null,
+  joined: null,
+  pending: null,
+};
+
+export default user;
